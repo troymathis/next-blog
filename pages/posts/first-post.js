0bcac525ca6bc2b8aaa7a8d9ae6f098dfdc65759
Troy Mathis <troy.mathis00@gmail.com>
2023-05-06 08:13:26 -0500

    global css, passing css down by classname

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
 import Image from "next/image";
+import Script from "next/script";
 
 export default function FirstPost() {
     const YourComponent = () => {
@@ -11,14 +13,18 @@ export default function FirstPost() {
             alt="Troy!" />;
     };
     return (
-        <>
-        <Head>
-            <title>First Post</title>
-        </Head>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() => console.log('script loaded correctly, window.FB has been populated')} />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
