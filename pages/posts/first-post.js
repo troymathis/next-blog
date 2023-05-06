4bfb22454716d9c3c99eaa1ab12e8972434d9110
Troy Mathis <troy.mathis00@gmail.com>
2023-05-06 07:53:09 -0500

    next images and head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );
+    const YourComponent = () => {
+        <Image
+            src="/images/headshot.jpeg"
+            height={144}
+            width={144}
+            alt="Troy!" />;
+    };
+    return (
+        <>
+        <Head>
+            <title>First Post</title>
+        </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
 }
