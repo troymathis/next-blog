35133c48e4b77586fc10c67c74e87eef971289a3
Troy Mathis <troy.mathis00@gmail.com>
2023-05-06 16:52:46 -0500

    utilStyles added to post pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,33 +2,41 @@
 // getStaticPaths retrieve all possible values for the id
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    // retrieves post data and returns it as a prop
-    return {
-        props: {
-            postData,
-        }
-    }
+  const postData = await getPostData(params.id);
+  // retrieves post data and returns it as a prop
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
-    return (<Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
-    );
+  );
 }
